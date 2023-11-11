60bda690c44fa06afae1321d8b3e663d824c9e9e
tanhaok <haonguyentan2001@gmail.com>
2023-11-11 21:50:49 +0700

    initial project

---
+++ blogs-api/src/test/java/com/thebasics/blogsapi/BlogsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thebasics.blogsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
